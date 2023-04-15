8dfbd1ec4d5af9110fee3911e842a8e846c60287
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-04-15 23:10:38 +0100

    Setup MongoDB connection

--- 0-feedback_form/abiodun_feedback_form/backend/index.js
+++ 0-feedback_form/abiodun_feedback_form/backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { initiateDb } from "./src/database/db.js";
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,12 @@ const { PORT } = process.env || 6000;
 
 app.use(express.json());
 app.use(cors());
+
+initiateDb();
+
+app.get('/api/test', (req, res) => {
+	return res.status(200).json({message: "FeedBack service is available!!!"})
+})
 
 app.listen(PORT, () => {
 	console.log(`Server listening on http://localhost:${PORT}`);
