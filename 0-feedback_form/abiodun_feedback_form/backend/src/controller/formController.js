941a54c195b9bb9598710210cecb217e4006a1ab
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-04-16 13:49:55 +0100

    Prevent duplicate feedback for same email and event type

--- 0-feedback_form/abiodun_feedback_form/backend/src/controller/formController.js
+++ 0-feedback_form/abiodun_feedback_form/backend/src/controller/formController.js
@@ -5,11 +5,11 @@ export const postFeedback = async (req, res) => {
 	try {
 		const { name, email, cohort, sex, date, event_type, feedback } =
 			req.body;
-		const checkIfEmailExists = await Form.findOne({ email });
-		if (checkIfEmailExists) {
+		const existingFeedback = await Form.findOne({ email, event_type });
+		if (existingFeedback) {
 			return res
 				.status(409)
-				.json({ message: "Email has already posted a feedback" });
+				.json({ message: "Your Feedback has already been taken" });
 		}
 		const newFeedback = await Form.create({
 			name,
@@ -20,12 +20,10 @@ export const postFeedback = async (req, res) => {
 			event_type,
 			feedback,
 		});
-		return res
-			.status(201)
-			.json({
-				message: "Feedback created successfully",
-				data: newFeedback,
-			});
+		return res.status(201).json({
+			message: "Feedback created successfully",
+			data: newFeedback,
+		});
 	} catch (error) {
 		console.log(`Error creating Feedback: ${error.message}`);
 	}
